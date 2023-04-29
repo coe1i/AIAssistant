fcf984f0a0f4d34946eede606c20b22e4cd00541
Usuario <Usuario@DESKTOP-A058HV1>
2023-04-29 17:23:09 +0200

    Agregar archivos de proyecto.

---
+++ Controllers/ChatGPTController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using openai.Recursos.ChatGPT;
+using RestSharp;
+using System.Text.Json.Serialization;
+
+namespace openai.Controllers
+{
+    public class ChatGPTController : Controller
+    {
+        public static string _EndPoint = "https://api.openai.com/";
+        public static string _URI = "v1/chat/completions";
+        //integrar api en ssm parameterstore (opcion 1)
+        /// <summary>
+        /// pasarle las variables de entorno a ebs, formato de clave - val y cifrado
+        /// </summary>
+        public static string _APIKey = "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";
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string pSolicitud)
+        {
+            var strRespuesta = "";
+
+            //Consumir la API
+            var oCliente = new RestClient(_EndPoint);
+            var oSolicitud = new RestRequest(_URI, Method.Post);
+            oSolicitud.AddHeader("Content-Type", "application/json");
+            oSolicitud.AddHeader("Authorization", "Bearer "+_APIKey);
+           
+            //Creamos el cuerpo de la solicitud
+            var oCuerpo = new Request()
+            {
+                model = "gpt-3.5-turbo",
+                messages= new List<Message>()
+                {
+                    new Message() {
+                        role="user", 
+                        content=pSolicitud
+                    }
+                }
+            };
+
+            var jsonString = JsonConvert.SerializeObject(oCuerpo);
+
+            oSolicitud.AddJsonBody(jsonString);
+
+            var oRespuesta = oCliente.Post<Response>(oSolicitud);
+
+            strRespuesta = oRespuesta.choices[0].message.content;
+
+            ViewBag.Respuesta= strRespuesta; 
+
+            return View();
+        }
+    }
+}
